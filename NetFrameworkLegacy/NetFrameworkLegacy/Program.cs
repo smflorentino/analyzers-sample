5d10e4a4534b7173956504f6607362fb119744eb
Scott Florentino <smflorentino@users.noreply.github.com>
2020-07-02 15:41:45 -0700

    initial code commit

---
+++ NetFrameworkLegacy/NetFrameworkLegacy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrameworkLegacy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeObject someObject = new SomeObject(42);
+            Console.WriteLine($"Hello, World: {someObject}");
+        }
+    }
+}
---
+++ NetFrameworkLegacy/NetFrameworkLegacy/SomeObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrameworkLegacy
+{
+    class SomeObject
+    {
+        public SomeObject(int value)
+        {
+            IntField = value;
+            StringField = value.ToString();
+        }
+
+        public string StringField { get; set; }
+
+        public bool BoolField { get; set; }
+
+        public int IntField { get; set; }
+
+        public override string ToString()
+        {
+            return StringField;
+        }
+    }
+}
